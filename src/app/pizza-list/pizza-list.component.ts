ffc76cc31b1c1eb46d0bfd65d5d838f3c135bd27
Pierre Bretéché <pbreteche@dawan.fr>
2019-09-03 16:44:21 +0200

    add a filter in nav list

--- src/app/pizza-list/pizza-list.component.ts
+++ src/app/pizza-list/pizza-list.component.ts
@@ -10,19 +10,39 @@ import {pizze} from '../data/pizza-data';
 export class PizzaListComponent implements OnInit {
 
   pizze: Pizza[] = [];
+  filteredPizze: Pizza[] = [];
   @Output()
   pizzaSelected = new EventEmitter<Pizza>();
   currentPizza: Pizza;
+  vegeFilter = 'all';
 
   constructor() { }
 
   ngOnInit() {
     this.pizze = pizze;
+    this.filteredPizze = pizze;
     this.currentPizza = pizze[0];
+  }
+
+  private pizzeFilter() {
+    this.filteredPizze = this.pizze.filter(pizza => {
+      return this.vegeFilter === 'all' ||
+        (pizza.veganFriendly && this.vegeFilter === 'vege') ||
+        (!pizza.veganFriendly && this.vegeFilter === 'no-vege');
+    });
+
+    if (this.filteredPizze.indexOf(this.currentPizza) === -1) {
+      this.select(this.filteredPizze[0]);
+    }
   }
 
   select(pizza: Pizza) {
     this.currentPizza = pizza;
     this.pizzaSelected.emit(pizza);
+  }
+
+  setVegeFilter(value: string) {
+    this.vegeFilter = value;
+    this.pizzeFilter();
   }
 }
